67ee559f4d142e9212c689650d175000f36371b5
ilyesX10 <ilyesdrouiche00@gmail.com>
2026-05-22 23:40:56 +0100

    11

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
   return (
     <>
       <div dir={`${lang == "Ar"? "rtl":"ltr"}`} className={`${lang =="Ar"? "font-[ArabicFont]":"font-[LatinFont]"}`}>
-        <Navbar/>
         <Header/>
           <main>
             <Roadmap/>
