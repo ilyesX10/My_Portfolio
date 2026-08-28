e4524a60b3bfaf5e84268013ed88a7ebbc6f9cad
ilyesX10 <83101572+ilyesX10@users.noreply.github.com>
2026-08-28 21:02:41 +0100

    add resume link

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,6 @@
 
 import myPhoto from "../assets/images/myPhoto.JPG"
-import { person,mySiteInfo } from "../infos";
+import { person,mySiteInfo,contactLinks } from "../infos";
 import { useContext, useState } from "react";
 import { ThemeContext } from "../ThemeContext";
 import { motion } from "motion/react"
@@ -34,7 +34,7 @@ export default function Header(){
                 </p>
                 <div className={`flex 2xs:flex-row flex-col gap-4 sm:text-xl text-sm my-2`}>
                     <motion.a onClick={handelScroll} whileTap={{scale:0.9}} transition={{type:"spring"}} className="sm:text-base text-xs px-3 py-2 text-gray-300 dark:bg-slate-800 bg-indigo-500 hover:cursor-pointer rounded-lg border-r-6 border-b-6 border-indigo-900 dark:border-slate-900" type="button" onMouseEnter={()=>{setHoverBtn1(prev=>true)}} onMouseLeave={()=>{setHoverBtn1(prev=>false)}}>{mySiteInfo[lang].headerBtn[0]} <span className={`inline-block relative transition-all ${lang !="Ar"? isHoverBtn1 ? "left-1":"left-0" : isHoverBtn1 ? "right-1":"right-0"}`}>{lang != "Ar"? "→" : "←"}</span></motion.a>
-                    <motion.a whileTap={{scale:0.9}} transition={{type:"spring"}} className="sm:text-base text-xs px-3 py-2 text-gray-300 dark:bg-slate-800 bg-indigo-500 hover:cursor-pointer rounded-lg border-r-6 border-b-6 border-indigo-900 dark:border-slate-900" type="button" onMouseEnter={()=>{setHoverBtn2(prev=>true)}} onMouseLeave={()=>{setHoverBtn2(prev=>false)}}>{mySiteInfo[lang].headerBtn[1]} <span className={`inline-block relative transition-all ${lang !="Ar"? isHoverBtn2 ? "left-1":"left-0" : isHoverBtn2 ? "right-1":"right-0"}`}>{lang != "Ar"? "→" : "←"}</span></motion.a>
+                    <motion.a href={contactLinks.resume} whileTap={{scale:0.9}} transition={{type:"spring"}} className="sm:text-base text-xs px-3 py-2 text-gray-300 dark:bg-slate-800 bg-indigo-500 hover:cursor-pointer rounded-lg border-r-6 border-b-6 border-indigo-900 dark:border-slate-900" type="button" onMouseEnter={()=>{setHoverBtn2(prev=>true)}} onMouseLeave={()=>{setHoverBtn2(prev=>false)}}>{mySiteInfo[lang].headerBtn[1]} <span className={`inline-block relative transition-all ${lang !="Ar"? isHoverBtn2 ? "left-1":"left-0" : isHoverBtn2 ? "right-1":"right-0"}`}>{lang != "Ar"? "→" : "←"}</span></motion.a>
                 </div>
             </motion.div>
             <div className={`lg:col-span-1 col-span-3 flex flex-col gap-y-6 justify-center items-center`}>
